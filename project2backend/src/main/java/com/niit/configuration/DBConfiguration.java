e83716601eb5b692940dbfa38549922ece351b2d
divya naveen <divya naveen@DESKTOP-P8C9FLB>
2018-09-24 19:02:13 +0530

    project 2

--- project2backend/src/main/java/com/niit/configuration/DBConfiguration.java
+++ project2backend/src/main/java/com/niit/configuration/DBConfiguration.java
@@ -2,6 +2,7 @@ package com.niit.configuration;
 
 import java.util.Properties;
 
+
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
@@ -12,7 +13,7 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
+import com.niit.model.User;
 @Configuration
 @EnableTransactionManagement
 public class DBConfiguration 
@@ -31,7 +32,7 @@ public class DBConfiguration
 		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
 		hibernateProperties.setProperty("hibernate.show_sql", "true");
 		lsf.addProperties(hibernateProperties);
-		Class classes[]=new Class[] {};
+		Class classes[]=new Class[] {User.class};
 	    return lsf.addAnnotatedClasses(classes).buildSessionFactory();
 	}
 	@Bean
---
+++ project2middleware/src/main/java/com/niit/controllers/UserController.java
@@ -0,0 +1,72 @@
+package com.niit.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.dao.UserDao;
+import com.niit.model.ErrorClazz;
+import com.niit.model.User;
+
+@RestController
+public class UserController
+{
+	@Autowired
+	private UserDao userDao;
+	
+	public UserController()
+	{
+	System.out.println("UserController INSTANTIATED");
+}
+	@RequestMapping(value="/register",method=RequestMethod.POST)
+	public ResponseEntity<?> registration(@RequestBody User user) {
+		if(!userDao.isEmailValid(user.getEmail())) {
+			ErrorClazz error=new ErrorClazz(2,"Email id already exists...please enter different email id");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.CONFLICT);
+		}
+		try {
+			userDao.registration(user);
+			return new ResponseEntity<User>(user,HttpStatus.CREATED);
+		}
+		catch(Exception e) {
+			ErrorClazz error=new ErrorClazz(2,"Unable to register user details");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	//{'email':'john.s@abc.com','password':'123'}  - i/p
+	public ResponseEntity<?> login(@RequestBody User user,HttpSession session){
+		User validUser=userDao.login(user);
+		if(validUser==null)//invalid credentials, error
+		{
+			ErrorClazz error=new ErrorClazz(3,"Invalid email id/password.. please enter valid credentials");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.UNAUTHORIZED);//401
+		}
+		else{//valid credentials, success
+			validUser.setOnline(true);
+			userDao.update(validUser);
+			session.setAttribute("loginId",validUser.getEmail());
+			return new ResponseEntity<User>(validUser,HttpStatus.OK);
+		}
+	}
+	
+	
+	@RequestMapping(value="/getuser",method=RequestMethod.GET)
+	public ResponseEntity<?> getUser(HttpSession session)
+	{
+		String emailId=(String)session.getAttribute("loginId");
+		if(emailId==null){
+			ErrorClazz error=new ErrorClazz(4,"Unauthorized access.. Please login");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.UNAUTHORIZED);//401 error function
+		}
+
+		User user=userDao.getUser(emailId);
+		return new ResponseEntity<User>(user,HttpStatus.OK);//success function
+	}
+}
